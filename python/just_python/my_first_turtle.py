89e3821b694d439bd941eb362c66224ebddf5a6c
nikita351 <nikitamironov351@gmail.com>
2021-03-10 13:03:02 +0200

    my_python

---
+++ python/just_python/my_first_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.shapesize(2)
+turtle.color('red', 'green')
+
+for step in range(6):
+    
+    turtle.begin_fill()
+    for i in range(3):
+        turtle.forward(50)
+        turtle.left(360 / 3)
+    turtle.end_fill()
+
+    turtle.forward(50)
+    turtle.right(60)
